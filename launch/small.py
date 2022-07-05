7b682a66c4fb1027689ab505beeaa1427235c2ac
M2135 <martin.petri@gmx.net>
2022-07-05 15:38:56 +0200

    New Modbus address

--- launch/small.py
+++ launch/small.py
@@ -39,7 +39,7 @@ def generate_launch_description():
                 parameters=[
                         {"name": 'modbus_tcp_node'},
                         {"topic_name": 'json_modbus_values'},
-                        {"modbus_host_ip": '192.168.178.51'},
+                        {"modbus_host_ip": '192.168.51.102'},
                         {"modbus_host_port": 502},
                         {"modbus_service_name": MODBUS_SERVICE_NAME},
                         {"read_modbus_interval": 1},
