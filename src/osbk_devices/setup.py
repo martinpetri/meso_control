aabf330e237a66b37e511c96251e4689002768a5
Tim Galdiga <t.galdiga@uni-bremen.de>
2023-04-17 15:36:48 +0200

    added entrypoints

--- src/osbk_devices/setup.py
+++ src/osbk_devices/setup.py
@@ -20,6 +20,8 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+            'sensor_base = osbk_devices.sensor_base:main',
+            'actuator_base = osbk_devices.actuator_base:main',
         ],
     },
 )
