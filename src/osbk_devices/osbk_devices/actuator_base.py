c9f85559a31130ed0e8af981c731860598b534f5
Tim Galdiga <t.galdiga@uni-bremen.de>
2023-04-17 14:58:17 +0200

    added ActuatorBase node

---
+++ src/osbk_devices/osbk_devices/actuator_base.py
@@ -0,0 +1,38 @@
+import rclpy
+from rclpy.node import Node
+from awi_interfaces import ActuatorControl
+
+
+class ActuatorBase(Node):
+
+    def __init__(self, name):
+        super().__init__(name)
+
+        self.service_name = f'{name}/control'
+        self.srv = self.create_service(ActuatorControl, self.service_name, self.command_callback)
+    def __init__(self):
+        self.__init__('sensor')
+        
+
+
+    def command_callback(self, request, response):
+        response = self.set_actuator(request)
+        return response
+    
+    def set_actuator(self, setpoint):
+        return None
+
+
+def main(args=None):
+    rclpy.init(args=args)
+
+    actuator = ActuatorBase()
+
+    rclpy.spin(actuator)
+    
+    actuator.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
